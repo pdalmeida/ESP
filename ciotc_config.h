6a2451f6b89ecdf723ddb1bdfd070ebf4be7637f
Lucas-Baptista <80826180+LLB99@users.noreply.github.com>
2022-12-09 19:09:39 +0000

    Add files via upload

---
+++ ciotc_config.h
@@ -0,0 +1,98 @@
+/******************************************************************************
+ * Copyright 2018 Google
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+// This file contains your configuration used to connect to Cloud IoT Core
+
+// WIFI
+// const char* ssid = "SFR A88D_CLOUD";
+// const char* password = "123456789";
+
+const char* ssid = "Vodafone-538FDC";
+const char* password = "7CPs4PYjQ2";
+
+// Cloud iot details.
+/*proj_Lucas
+const char* project_id = "teste-esp8266-lucas";
+const char* location = "europe-west1";
+const char* registry_id = "atest-registry";
+const char* device_id = "atest-dev";
+*/
+
+const char* project_id = "buoyant-dynamo-366609";
+const char* location = "europe-west1";
+const char* registry_id = "ESP_reg";
+const char* device_id = "ESP_1";
+
+// Configuration for NTP
+const char* ntp_primary = "time.google.com";
+const char* ntp_secondary = "pool.ntp.org";
+
+// To get the private key run (where private-key.pem is the ec private key
+// used to create the certificate uploaded to google cloud iot):
+// openssl ec -in <private-key.pem> -noout -text
+// and copy priv: part.
+// The key length should be exactly the same as the key length bellow (32 pairs
+// of hex digits). If it's bigger and it starts with "00:" delete the "00:". If
+// it's smaller add "00:" to the start. If it's too big or too small something
+// is probably wrong with your key.
+
+/*proj_Lucas
+const unsigned char private_key[] = {
+  0x60, 0xd5, 0x63, 0x8a, 0xef, 0xde, 0xf3, 0xc2, 0x33, 0x22, 0xe1, 0x39, 0x0f, 0x80, 0x4d,
+  0x7b, 0xe2, 0xf0, 0x33, 0xf9, 0xb9, 0x08, 0x51, 0x67, 0xd5, 0x8f, 0xd1, 0x10, 0xd5, 0xc9,
+  0xf6, 0x1e};*/
+  
+const unsigned char private_key[] = {
+  0x7e, 0x4a, 0x8c, 0x5a, 0x68, 0xfc, 0x51, 0x4c, 0x68, 0xc9, 0xdd, 0x0e, 0xc1, 0x42, 0x90,
+  0xb9, 0x91, 0x82, 0xcf, 0xff, 0x4a, 0x6b, 0x0a, 0x6c, 0x1f, 0xbf, 0x2c, 0x71, 0x45, 0xfa,
+  0x13, 0x8e};
+
+// Time (seconds) to expire token += 20 minutes for drift
+const int jwt_exp_secs = 3600; // Maximum 24H (3600*24)
+
+// Certificates for SSL on the LTS server
+const char* primary_ca = "-----BEGIN CERTIFICATE-----\n"
+    "MIIBxTCCAWugAwIBAgINAfD3nVndblD3QnNxUDAKBggqhkjOPQQDAjBEMQswCQYD\n"
+    "VQQGEwJVUzEiMCAGA1UEChMZR29vZ2xlIFRydXN0IFNlcnZpY2VzIExMQzERMA8G\n"
+    "A1UEAxMIR1RTIExUU1IwHhcNMTgxMTAxMDAwMDQyWhcNNDIxMTAxMDAwMDQyWjBE\n"
+    "MQswCQYDVQQGEwJVUzEiMCAGA1UEChMZR29vZ2xlIFRydXN0IFNlcnZpY2VzIExM\n"
+    "QzERMA8GA1UEAxMIR1RTIExUU1IwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAATN\n"
+    "8YyO2u+yCQoZdwAkUNv5c3dokfULfrA6QJgFV2XMuENtQZIG5HUOS6jFn8f0ySlV\n"
+    "eORCxqFyjDJyRn86d+Iko0IwQDAOBgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUw\n"
+    "AwEB/zAdBgNVHQ4EFgQUPv7/zFLrvzQ+PfNA0OQlsV+4u1IwCgYIKoZIzj0EAwID\n"
+    "SAAwRQIhAPKuf/VtBHqGw3TUwUIq7TfaExp3bH7bjCBmVXJupT9FAiBr0SmCtsuk\n"
+    "miGgpajjf/gFigGM34F9021bCWs1MbL0SA==\n"
+    "-----END CERTIFICATE-----\n";
+
+const char* backup_ca = "-----BEGIN CERTIFICATE-----\n"
+    "MIIB4TCCAYegAwIBAgIRKjikHJYKBN5CsiilC+g0mAIwCgYIKoZIzj0EAwIwUDEk\n"
+    "MCIGA1UECxMbR2xvYmFsU2lnbiBFQ0MgUm9vdCBDQSAtIFI0MRMwEQYDVQQKEwpH\n"
+    "bG9iYWxTaWduMRMwEQYDVQQDEwpHbG9iYWxTaWduMB4XDTEyMTExMzAwMDAwMFoX\n"
+    "DTM4MDExOTAzMTQwN1owUDEkMCIGA1UECxMbR2xvYmFsU2lnbiBFQ0MgUm9vdCBD\n"
+    "QSAtIFI0MRMwEQYDVQQKEwpHbG9iYWxTaWduMRMwEQYDVQQDEwpHbG9iYWxTaWdu\n"
+    "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEuMZ5049sJQ6fLjkZHAOkrprlOQcJ\n"
+    "FspjsbmG+IpXwVfOQvpzofdlQv8ewQCybnMO/8ch5RikqtlxP6jUuc6MHaNCMEAw\n"
+    "DgYDVR0PAQH/BAQDAgEGMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFFSwe61F\n"
+    "uOJAf/sKbvu+M8k8o4TVMAoGCCqGSM49BAMCA0gAMEUCIQDckqGgE6bPA7DmxCGX\n"
+    "kPoUVy0D7O48027KqGx2vKLeuwIgJ6iFJzWbVsaj8kfSt24bAgAXqmemFZHe+pTs\n"
+    "ewv4n4Q=\n"
+    "-----END CERTIFICATE-----\n";
+
+// In case we ever need extra topics
+const int ex_num_topics = 0;
+const char* ex_topics[ex_num_topics];
+//const int ex_num_topics = 1;
+//const char* ex_topics[ex_num_topics] = {
+//  "/devices/my-device/tbd/#"
+//};
